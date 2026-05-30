e3419cc45c503d67c9a0aa4ab178053bc7adee3f
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-05-30 14:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 var isDate = function (input) {
   //   write your code here
-};
+  if(input instanceof Date){
+		return !isNan(input.getTime());
+  }
 
+  if(Array.isArray(input) || input == NULL){
+	  return false;
+  }
+
+  const timestamp  = Date.parse(input);
+
+  if(isNan(timestamp)){
+	  return typeof(input) !== 'boolean' && !(isNan(Number(input)));
+  }
+
+	return true;
+}
 // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
